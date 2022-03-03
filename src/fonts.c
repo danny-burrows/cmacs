d9b0050dbad5ce0a52282d067c5816561ca2e32a
danny-burrows <dannyburrows@protonmail.com>
2022-03-03 13:41:01 +0000

    Moving font to assets

--- src/fonts.c
+++ src/fonts.c
@@ -3,9 +3,9 @@
 Fonts fonts;
 
 int load_fonts(void) {
-    fonts.font_regular = TTF_OpenFont("open_sans.ttf", 15);
-    fonts.font_large = TTF_OpenFont("open_sans.ttf", 20);
-    fonts.font_larger = TTF_OpenFont("open_sans.ttf", 64);
+    fonts.font_regular = TTF_OpenFont("assets/open_sans.ttf", 15);
+    fonts.font_large = TTF_OpenFont("assets/open_sans.ttf", 20);
+    fonts.font_larger = TTF_OpenFont("assets/open_sans.ttf", 64);
     if (fonts.font_regular == NULL || fonts.font_large == NULL || fonts.font_larger == NULL) {
         return -1;
     }
