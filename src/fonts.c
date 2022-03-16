e0b1ad9fe03d50646e0e6a1539f77ce6a198b6b4
danny-burrows <dannyburrows@protonmail.com>
2022-03-16 18:24:36 +0000

    Changing to monoid for text

--- src/fonts.c
+++ src/fonts.c
@@ -3,7 +3,7 @@
 Fonts fonts;
 
 int load_fonts(void) {
-    fonts.font_mono_regular = TTF_OpenFont("assets/roboto-mono-regular.ttf", 15);
+    fonts.font_mono_regular = TTF_OpenFont("assets/monoid-reg.ttf", 12);
     fonts.font_regular = TTF_OpenFont("assets/open-sans.ttf", 15);
     fonts.font_large = TTF_OpenFont("assets/open-sans.ttf", 20);
     fonts.font_larger = TTF_OpenFont("assets/open-sans.ttf", 64);
