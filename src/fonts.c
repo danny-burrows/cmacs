f329a0a7d98497a6f86fc2b9c84daa5ea44e3263
danny-burrows <dannyburrows@protonmail.com>
2022-03-16 18:27:05 +0000

    Error checking in fonts.c

--- src/fonts.c
+++ src/fonts.c
@@ -2,13 +2,19 @@
 
 Fonts fonts;
 
-int load_fonts(void) {
+int load_fonts(void) 
+{
     fonts.font_mono_regular = TTF_OpenFont("assets/monoid-reg.ttf", 12);
+    if (fonts.font_mono_regular == NULL) return -1;
+
     fonts.font_regular = TTF_OpenFont("assets/open-sans.ttf", 15);
+    if (fonts.font_regular == NULL) return -1;
+
     fonts.font_large = TTF_OpenFont("assets/open-sans.ttf", 20);
+    if (fonts.font_large == NULL) return -1;
+
     fonts.font_larger = TTF_OpenFont("assets/open-sans.ttf", 64);
-    if (fonts.font_regular == NULL || fonts.font_large == NULL || fonts.font_larger == NULL) {
-        return -1;
-    }
+    if (fonts.font_larger == NULL) return -1;
+
     return 0;
 }
