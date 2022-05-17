dd4f2be4bb86617cd40df1b58670aa408240ec5b
AnnabethS <annabeths111@gmail.com>
2022-05-17 15:12:53 +0100

    added language layer

---
+++ src/cmacs_layer.h
@@ -0,0 +1,19 @@
+#include <stdint.h>
+
+#ifndef CMACS_LAYER
+#define CMACS_LAYER
+
+#define local static
+
+#define u8 uint8_t
+#define u16 uint16_t
+#define u32 uint32_t
+#define u64 uint64_t
+
+#define i8 int8_t
+#define i16 int16_t
+#define i32 int32_t
+#define i64 int64_t
+
+#define uchar unsigned char
+#endif
